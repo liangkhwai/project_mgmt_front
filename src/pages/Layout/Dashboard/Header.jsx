6f19389cfa80623e7f3c30ce014a1c494e0ef5c2
liangkhwai <liangkhwai@gmail.com>
2023-04-19 13:57:27 +0700

    add header layout

--- src/pages/Layout/Dashboard/Header.jsx
+++ src/pages/Layout/Dashboard/Header.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { AiOutlineMenuUnfold } from "react-icons/ai/index";
 const Header = ({ openHandler }) => {
   return (
-    <div className="shadow-md flex item-center h-14">
-      <button onClick={openHandler} className="ml-5">
-        <AiOutlineMenuUnfold size="2em" color="grey" />
-      </button>
-      <div className="flex items-center align-middle">Header</div>
+    <div className="shadow-md flex item-center h-14 justify-between">
+      <div className="flex items-center gap-4">
+        <button onClick={openHandler} className="ml-5">
+          <AiOutlineMenuUnfold size="2em" color="grey" />
+        </button>
+        <div className="">Search input</div>
+      </div>
+      <div className="flex items-center gap-4">
+        <div>NotificationsMenu</div>
+        <div className="mr-5">AvatarMenu</div>
+      </div>
     </div>
   );
 };
