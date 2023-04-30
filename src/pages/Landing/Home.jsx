6cc7d0c4065e522febda535762f4fadb54ac28bc
ChanoknanCM <132209563+ChanoknanCM@users.noreply.github.com>
2023-05-01 01:43:26 +0700

    Update Home.jsx
    เอา text ไว้ในภาพไม่ได้ TvT เเบบร่างไปก่อนละกานนะฮะ

--- src/pages/Landing/Home.jsx
+++ src/pages/Landing/Home.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import univer3 from "../../assets/univer3.png";
 import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-   <div>
-    Home
-   </div>
+    <div className="w-screen h-screen bg-blue-50">
+           <img          
+            src={univer3}
+            alt="univer"
+            style={{ width: "100%", height: "auto" }}
+
+          />
+    <div className=" w-screen h-screen bg-blue-50 ">
+      <div className=" text-xs ml-40 mt-10 ">ข้อมูล.....</div>
+      <div className="text-lg font-medium ml-40 ">สถิติเจ้าของผลงาน</div>
+     
+
+      <div className=" text-sky-500  grid grid-cols-3 gap-96 mt-10 ml-60 mr-60  ">
+        <div className="text-4xl font-medium   ">1,205</div>
+        <div className="text-4xl font-medium   ">2,205</div>
+        <div className="text-4xl font-medium   ">205</div>
+      </div>
+      <div className="  text-black text-justify grid grid-cols-3 gap-96  ml-56 mr-60  ">
+        <div className="text-sm font-medium   ">จำนวนผลงานวิจัย</div>
+        <div className="text-sm font-medium   ">จำนวนผลงานวิจัย</div>
+        <div className="text-sm font-medium   ">จำนวนผลงานวิจัย</div>
+      </div>
+    </div>
+    </div>
   );
 };
 
