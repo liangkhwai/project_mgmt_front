87b3abf4b10cae055959aade70fe5a5672dc7e4b
ChanoknanCM <132209563+ChanoknanCM@users.noreply.github.com>
2023-05-01 13:56:05 +0700

    Update Home.jsx
    Co-Authored-By: suphawat fatwiset <30294056+liangkhwai@users.noreply.github.com>
    Co-Authored-By: Meldyjuju <128712254+Meldyjuju@users.noreply.github.com>

--- src/pages/Landing/Home.jsx
+++ src/pages/Landing/Home.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import univer3 from "../../assets/univer3.png";
+import univer3 from "../../assets/univer2.png";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="w-screen h-screen bg-blue-50">
+    <div className=" bg-blue-50">
            <img          
             src={univer3}
-            alt="univer"
+            alt="dsadsa"
             style={{ width: "100%", height: "auto" }}
 
           />
-    <div className=" w-screen h-screen bg-blue-50 ">
-      <div className=" text-xs ml-40 mt-10 ">ข้อมูล.....</div>
-      <div className="text-lg font-medium ml-40 ">สถิติเจ้าของผลงาน</div>
+    <div className=" w-full h-screen bg-blue-50 ">
+      <div className="text-xs ml-40 mt-10 ">ข้อมูล............</div>
+      <div className="text-2xl font-medium ml-40 ">สถิติเจ้าของผลงาน</div>
      
 
-      <div className=" text-sky-500  grid grid-cols-3 gap-96 mt-10 ml-60 mr-60  ">
-        <div className="text-4xl font-medium   ">1,205</div>
-        <div className="text-4xl font-medium   ">2,205</div>
-        <div className="text-4xl font-medium   ">205</div>
+      <div className=" text-center  w-auto flex mt-10">
+        <div className="text-sky-500 text-4xl font-medium flex-1 ">1,049</div>
+        <div className="text-sky-500 text-4xl font-medium flex-1  ">689</div>
+        <div className="text-sky-500 text-4xl font-medium flex-1 ">79</div>
       </div>
-      <div className="  text-black text-justify grid grid-cols-3 gap-96  ml-56 mr-60  ">
-        <div className="text-sm font-medium   ">จำนวนผลงานวิจัย</div>
-        <div className="text-sm font-medium   ">จำนวนผลงานวิจัย</div>
-        <div className="text-sm font-medium   ">จำนวนผลงานวิจัย</div>
+      <div className="text-center w-auto flex mt-2 ">
+        <div className="w-80  text-sm font-medium  flex-1">จำนวนผลงานวิจัย</div>
+        <div className="w-80  text-sm font-medium  flex-1">จำนวนผู้วิจัยที่สำเร็จการศึกษา</div>
+        <div className="w-80  text-sm font-medium  flex-1">จำนวนผู้วิจัยที่กำลังศึกษาอยู่</div>
       </div>
     </div>
     </div>
