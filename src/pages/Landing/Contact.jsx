45317b16a6f7f4342a03260ea0efbb959aaa1e74
ChanoknanCM <132209563+ChanoknanCM@users.noreply.github.com>
2023-05-01 16:59:48 +0700

    Revert "Merge branch 'main' of https://github.com/liangkhwai/project_mgmt_front"
    This reverts commit 196fe59d5d783baf49f3d150604fd14cd28d7f27, reversing
    changes made to 52c9fc165a42e3eb1dc36cc9eebeff83989e495e.

--- src/pages/Landing/Contact.jsx
+++ src/pages/Landing/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
           <iframe
             title="map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1913.4661528196496!2d102.86230943879478!3d16.42826406878465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31228bda829566e7%3A0x24ca9da06e436ef3!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LmA4LiX4LiE4LmC4LiZ4LmC4Lil4Lii4Li14Lij4Liy4LiK4Lih4LiH4LiE4Lil4Lit4Li14Liq4Liy4LiZIOC4p-C4tOC4l-C4ouC4suC5gOC4guC4leC4guC4reC4meC5geC4geC5iOC4mQ!5e0!3m2!1sth!2sth!4v1682871832375!5m2!1sth!2sth"
-            width="1450"
+            className="w-full"
             height="650"
             style={{ border: 0, margin: "auto" }}
             allowFullScreen=""
