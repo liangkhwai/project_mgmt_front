4203417576b8c2fbba58b31d991fb0ba827820a6
ChanoknanCM <132209563+ChanoknanCM@users.noreply.github.com>
2023-05-02 23:50:18 +0700

    สร้างภาพสไลด์ในหน้า home (ยังไม่เสร็จ)

--- src/pages/Landing/Home.jsx
+++ src/pages/Landing/Home.jsx
@@ -1,16 +1,78 @@
 import React from "react";
-import univer2 from "../../assets/univer2.png";
+import univer1 from "../../assets/univer1.png";
+import univer0 from "../../assets/univer0.png";
+import univer4 from "../../assets/univer4.png";
 import { Link } from "react-router-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const Home = () => {
   return (
     <div className=" bg-blue-50">
-      <img
-        src={univer2}
-        alt="dsadsa"
-        style={{ width: "100%", height: "auto" }}
-      />
-      
+      <div>
+        <Carousel
+          autoPlay={true}
+          showThumbs={false}
+          showStatus={false}
+          interval={1000}
+          transitionTime={600}
+          infiniteLoop={true}
+          showArrows={false}
+          dynamicHeight ={true}
+          stopOnHover={true}
+          
+        >
+          <div className="">
+            <div className="relative">
+              <img
+                className="h-full"
+                src={univer1}
+                alt="dsadsa"
+                style={{ width: "100%", height: "auto" }}
+              />
+            </div>
+            <div className="absolute bottom-0 right-0 text-5xl mb-20 mr-8 text-white  uppercase font-normal">
+              rmuti kkc
+            </div>
+            <div className="absolute bottom-0 right-0 text-5xl mb-9 mr-8 text-white font-normal ">
+              Digital Business Technology
+            </div>
+          </div>
+          <div  className="">
+            <div className="relative">
+              <img
+                className="h-full"
+                src={univer0}
+                alt="dsadsa"
+                style={{ width: "100%", height: "auto" }}
+              />
+            </div>
+            <div className="absolute bottom-0 right-0 text-5xl mb-20 mr-8 text-white  uppercase font-normal">
+              rmuti kkc
+            </div>
+            <div className="absolute bottom-0 right-0 text-5xl mb-9 mr-8 text-white font-normal ">
+              Digital Business Technology
+            </div>
+          </div>
+          {/* <div className="">
+            <div className="relative border border-black  h-full ">
+              <img
+                className="h-"
+                src={univer4}
+                alt="dsadsa"
+                style={{ width: "100%", height: "auto" }}
+              />
+            </div>
+            <div className="absolute bottom-0 right-0 text-5xl mb-20 mr-8 text-white  uppercase font-normal">
+              rmuti kkc
+            </div>
+            <div className="absolute bottom-0 right-0 text-5xl mb-9 mr-8 text-white font-normal ">
+              Digital Business Technology
+            </div>
+          </div> */}
+        </Carousel>
+      </div>
+
       <div className=" w-full h-56 bg-blue-50 ">
         <div className="text-xs ml-40 mt-10 ">ข้อมูล............</div>
         <div className="text-2xl font-medium ml-40 ">สถิติเจ้าของผลงาน</div>
--- src/pages/Landing/Thesis.jsx
+++ src/pages/Landing/Thesis.jsx
@@ -16,6 +16,7 @@ const Thesis = () => {
         </div>
       </div>
     </div>
+    
   );
 };
 
