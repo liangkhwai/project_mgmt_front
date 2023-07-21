3e8852ee4caf9aafe0db359055043f08ec9e6809
liangkhwai <liangkhwai@gmail.com>
2023-07-22 01:10:20 +0700

    fix bug in calendar

--- src/pages/Dashboard/Teacher/Calendar/Calendar.jsx
+++ src/pages/Dashboard/Teacher/Calendar/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useMemo } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -7,11 +7,14 @@ import listPlugin from "@fullcalendar/list";
 import Title from "../../../../UI/Title";
 import Body from "../../../../UI/Body";
 import Modal from "../../../../UI/Modal";
+import ModalContent from "./components/ModalContent";
+import dayjs from "dayjs";
+import thLocale from '@fullcalendar/core/locales/th';
 
 const Calendar = () => {
   const [isModalOpen, setModalOpen] = useState(false);
-  const [selectedDate, setSelectedDate] = useState([]);
-  const [events,setEvents] = useState([])
+  const [selectedDate, setSelectedDate] = useState();
+  const [events, setEvents] = useState([]);
   const handleOpenModal = (date) => {
     setModalOpen(true);
   };
@@ -20,31 +23,101 @@ const Calendar = () => {
     setModalOpen(false);
   };
 
-  const headerToolbar = {
-    left: "prev,next today",
-    center: "title",
-    right: "dayGridMonth,timeGridWeek,timeGridDay,list",
-  };
+  const headerToolbar = useMemo(() => {
+    return {
+      left: "prev,next today",
+      center: "title",
+      right: "dayGridMonth,timeGridWeek,timeGridDay,list",
+    };
+  }, []);
   const calendarRef = useRef(null);
 
-//   useEffect(() => {
-//     const calendarApi = calendarRef.current.getApi();
-//     calendarApi.setOption("selectable", true);
-//     calendarApi.setOption("selectMirror", true);
-//     calendarApi.setOption("select", handleDateSelect);
-//   }, []);
+  //   useEffect(() => {
+  //     const calendarApi = calendarRef.current.getApi();
+  //     calendarApi.setOption("selectable", true);
+  //     calendarApi.setOption("selectMirror", true);
+  //     calendarApi.setOption("select", handleDateSelect);
+  //   }, []);
 
   const handleSelect = (event) => {
+    console.log(event);
+    // const endDate = new Date(event.endStr);
+    // endDate.setDate(endDate.getDate() - 1);
+    // endDate.setHours(0);
+    // const subtractDate = endDate.toISOString().split("T")[0];
+    // console.log(subtractDate);
+
+    // const newDateEnd = new Date(subtractDate);
+
+    // const startTime = dayjs(event.start.toString()).set("hour", 12).$d;
+    let startTime = dayjs(event.start.toString()).$d;
+    let endTime = dayjs(event.end.toString()).$d;
+    let isAllDay = true;
+    if (event.view.type === "dayGridMonth") {
+      startTime = dayjs(event.start.toString()).set("hour", 12).$d;
+      endTime = dayjs(event.end.toString())
+        .subtract(1, "day")
+        .set("hour", 13).$d;
+    } else if (event.view.type === "timeGridWeek") {
+      isAllDay = false;
+    }
+    console.log(endTime);
+
     const newEvent = {
-      title: 'My Event',
-      start: event.start,
-      end: event.end,
+      title: "(ไม่มีชื่อ)",
+      start: startTime,
+      end: endTime,
+      allDay: isAllDay,
+    };
+
+    console.log(newEvent);
+    setSelectedDate(newEvent);
+    handleOpenModal();
+    // setEvents([...events, newEvent]);
+  };
+  const handleClick = (event) => {
+    console.log(event);
+
+    const newEvent = {  
+      title: "(ไม่มีชื่อ)",
+      date: event.date,
       allDay: event.allDay,
     };
-    setEvents([...events, newEvent]);
+    
+  };
+  const eventDidMount = (info) => {
+    // Modify the event format
+    // console.log(info);
+    // const eventEl = info.el;
+    // const eventTitle = info.event.title;
+    // const eventStart = dayjs(info.event.start).locale('th').format('HH:mm');
+    // const eventEnd = dayjs(info.event.end).locale('th').format('HH:mm');
+
+    // // Customize the event content, e.g., change the event title or add additional information
+    // // eventEl.innerHTML = `<strong>${eventTitle}</strong><br>Start: ${eventStart}<br>End: ${eventEnd}`;
+    // eventEl.innerHTML = `<strong>${eventTitle}</strong> &nbsp;${eventStart}`;
+    const { event } = info;
+    console.log(event);
+    const eventTitle = event.title;
+    const eventStart = dayjs(event.start).locale("th").format("HH:mm");
+    const eventEnd = dayjs(event.end).locale("th").format("HH:mm");
+
+    return {
+      html: `
+        <div class{${event.allDay === true && "bg-light-blue-600"}}}>
+          ${
+            event.allDay === false ? eventStart : ""
+          } &nbsp;<strong>${eventTitle}</strong> 
+        </div>
+      `,
+    };
   };
 
-  
+  const moreLinkContent = (event) => {
+    return {
+      html: `<strong className="m-2">อีก ${event.num} กิจกรรม</strong>`,
+    };
+  };
 
   return (
     <div className="mx-5">
@@ -53,9 +126,16 @@ const Calendar = () => {
         <div className="">
           <FullCalendar
             ref={calendarRef}
+            // timeZone="local"
             // views={{
             //   day: {
+            //     slotDuration: "00:30:00",
+            //     slotMinTime: "09:00:00",
+            //     slotMaxTime: "19:00:00",
+            //     selectMinDistance: 3,
             //     selectable: true,
+            //     selectMirror: true,
+            //     dateClick: handleClick,
             //   },
             //   week: {
             //     slotDuration: "00:30:00",
@@ -75,20 +155,29 @@ const Calendar = () => {
               listPlugin,
             ]}
             initialView="dayGridMonth"
-            dateClick={handleOpenModal}
+            // dateClick={handleClick}
+            selectMirror={true}
             headerToolbar={headerToolbar}
             selectable={true}
             weekends={true}
             select={handleSelect}
             events={events}
             slotEventOverlap={true}
-            eventMaxStack={5}
+            // eventMaxStack={5}
             dayMaxEvents={3}
-            />
-            
+            // defaultAllDay={false}
+            eventContent={eventDidMount}
+            moreLinkContent={moreLinkContent}
+            locale={thLocale}
+          />
+
           <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-            <div className="bg-blue-gray-200 z-auto text-white h-full w-full">
-              {selectedDate}
+            <div className=" z-auto h-full w-full  ">
+              <ModalContent
+                selectedDate={selectedDate}
+                setEvents={setEvents}
+                handleCloseModal={handleCloseModal}
+              />
             </div>
           </Modal>
         </div>
---
+++ src/pages/Dashboard/Teacher/Calendar/components/ModalContent.jsx
@@ -0,0 +1,338 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { AddButton } from "../../../../../UI/button";
+import dayjs from "dayjs";
+import "dayjs/locale/th";
+const ModalContent = ({ selectedDate, setEvents, handleCloseModal }) => {
+  const [date, setDate] = useState(selectedDate);
+  const [dateStart, setDateStart] = useState(date.start);
+  const [dateEnd, setDateEnd] = useState(date.end);
+  const [hourAm, setHourAm] = useState(12);
+  const [hourPm, setHourPm] = useState(13);
+  const [isAllDay, setIsAllDay] = useState(true);
+  const [isAddTime, setIsAddTime] = useState(false);
+ const pmDefault = [
+  {
+    name: "00.00",
+    val: 0,
+  },
+  {
+    name: "01.00",
+    val: 1,
+  },
+  {
+    name: "02.00",
+    val: 2,
+  },
+  {
+    name: "03.00",
+    val: 3,
+  },
+  {
+    name: "04.00",
+    val: 4,
+  },
+  {
+    name: "05.00",
+    val: 5,
+  },
+  {
+    name: "06.00",
+    val: 6,
+  },
+  {
+    name: "07.00",
+    val: 7,
+  },
+  {
+    name: "08.00",
+    val: 8,
+  },
+  {
+    name: "09.00",
+    val: 9,
+  },
+  {
+    name: "10.00",
+    val: 10,
+  },
+  {
+    name: "11.00",
+    val: 11,
+  },
+  {
+    name: "12.00",
+    val: 12,
+  },
+  {
+    name: "13.00",
+    val: 13,
+  },
+  {
+    name: "14.00",
+    val: 14,
+  },
+  {
+    name: "15.00",
+    val: 15,
+  },
+  {
+    name: "16.00",
+    val: 16,
+  },
+  {
+    name: "17.00",
+    val: 17,
+  },
+  {
+    name: "18.00",
+    val: 18,
+  },
+  {
+    name: "19.00",
+    val: 19,
+  },
+  {
+    name: "20.00",
+    val: 20,
+  },
+  {
+    name: "21.00",
+    val: 21,
+  },
+  {
+    name: "22.00",
+    val: 22,
+  },
+  {
+    name: "23.00",
+    val: 23,
+  },
+  {
+    name: "24.00",
+    val: 24,
+  },
+ ]
+  const [am, setAm] = useState([
+    {
+      name: "00.00",
+      val: 0,
+    },
+    {
+      name: "01.00",
+      val: 1,
+    },
+    {
+      name: "02.00",
+      val: 2,
+    },
+    {
+      name: "03.00",
+      val: 3,
+    },
+    {
+      name: "04.00",
+      val: 4,
+    },
+    {
+      name: "05.00",
+      val: 5,
+    },
+    {
+      name: "06.00",
+      val: 6,
+    },
+    {
+      name: "07.00",
+      val: 7,
+    },
+    {
+      name: "08.00",
+      val: 8,
+    },
+    {
+      name: "09.00",
+      val: 9,
+    },
+    {
+      name: "10.00",
+      val: 10,
+    },
+    {
+      name: "11.00",
+      val: 11,
+    },
+    {
+      name: "12.00",
+      val: 12,
+    },
+    {
+      name: "13.00",
+      val: 13,
+    },
+    {
+      name: "14.00",
+      val: 14,
+    },
+    {
+      name: "15.00",
+      val: 15,
+    },
+    {
+      name: "16.00",
+      val: 16,
+    },
+    {
+      name: "17.00",
+      val: 17,
+    },
+    {
+      name: "18.00",
+      val: 18,
+    },
+    {
+      name: "19.00",
+      val: 19,
+    },
+    {
+      name: "20.00",
+      val: 20,
+    },
+    {
+      name: "21.00",
+      val: 21,
+    },
+    {
+      name: "22.00",
+      val: 22,
+    },
+    {
+      name: "23.00",
+      val: 23,
+    },
+    {
+      name: "24.00",
+      val: 24,
+    },
+  ]);
+  const [pm, setPm] = useState([...pmDefault.filter((item)=> item.val >= hourPm)]);
+  useEffect(() => {
+    console.log(date);
+  }, [date]);
+
+  const startDate = dayjs(date.start.toString())
+    .locale("th")
+    .format("D MMMM YYYY");
+  const endDate = dayjs(date.end.toString()).locale("th").format("D MMMM YYYY");
+  const handleHourChangeAm = (event) => {
+    const dayjsStart = dayjs(date.start.toString()).set(
+      "hour",
+      event.target.value
+    );
+    console.log(dayjsStart);
+    setHourAm(event.target.value);
+    setDate((prev) => {
+      return { ...prev, start: dayjs(dayjsStart.$d).$d };
+    });
+    const filterMoreThanAm = pmDefault.filter(
+      (item) => item.val >= parseInt(event.target.value)
+    );
+
+    setPm([...filterMoreThanAm]);
+    // setDateStart(dayjsStart);
+  };
+  const handleHourChangePm = (event) => {
+    const dayjsEnd = dayjs(date.end.toString()).set("hour", event.target.value);
+    // console.log(dayjsEnd);
+    setHourPm(event.target.value);
+    setDate((prev) => {
+      return { ...prev, end: dayjs(dayjsEnd.$d).$d };
+    });
+    // setDateEnd(dayjsEnd);
+  };
+
+  const handleChangeCheckBox = () => {
+    setIsAllDay(!isAllDay);
+    setDate((prev) => {
+      return { ...prev, allDay: !isAllDay };
+    });
+  };
+
+  const submitHandlerEvent = () => {
+    console.log(date);
+    setEvents((prev) => [...prev, date]);
+    handleCloseModal();
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          name=""
+          id=""
+          className="w-full"
+          placeholder="เพิ่มชื่อและเวลา"
+          onChange={(e) =>
+            setDate((prev) => ({ ...prev, title: e.target.value }))
+          }
+        />
+      </div>
+      <div></div>
+      {startDate} - {endDate}{" "}
+      <button
+        className="border rounded-md p-1 text-gray-800 hover:bg-gray-100 text-sm"
+        onClick={() => setIsAddTime(!isAddTime)}
+      >
+        เพิ่มเวลา
+      </button>
+      {isAddTime && (
+        <Fragment>
+          <div>
+            <select onChange={handleHourChangeAm} value={hourAm}>
+              {am.map((item, idx) => {
+                return (
+                  <option key={idx} value={item.val}>
+                    {item.name}
+                  </option>
+                );
+              })}
+            </select>
+            -
+            <select onChange={handleHourChangePm} value={hourPm}>
+              {pm.map((item, idx) => {
+                return (
+                  <option key={idx} value={item.val}>
+                    {item.name}
+                  </option>
+                );
+              })}
+            </select>
+            &emsp;
+            <input
+              type="checkbox"
+              name=""
+              id=""
+              checked={isAllDay}
+              onChange={() => handleChangeCheckBox()}
+            />{" "}
+            <span>ตลอดทั้งวัน</span>
+          </div>
+        </Fragment>
+      )}
+      <div>
+        after Am : {hourAm}
+        <br />
+        after Pm : {hourPm}
+        <br />
+        after start : {dayjs(date.start.toString()).$d.toString()}
+        <br />
+        after start : {dayjs(date.end.toString()).$d.toString()}
+      </div>
+      <div className="my-1 text-center">
+        <AddButton onClick={() => submitHandlerEvent()}>บันทึก</AddButton>
+      </div>
+    </div>
+  );
+};
+
+export default ModalContent;
